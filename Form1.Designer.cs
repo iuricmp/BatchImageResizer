5a484bd1347e056ee83dd1d33ff45a4e1cff4562
Iuri Pereira <iuricmp@gmail.com>
2015-05-03 23:31:34 -0300

    new

---
+++ Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace BatchImageResizer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSelecionaPasta = new System.Windows.Forms.Button();
+            this.txtNomePasta = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnLerEstrutura = new System.Windows.Forms.Button();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblArquivosEncontrados = new System.Windows.Forms.Label();
+            this.btnIniciarReducao = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblQtdReduzidos = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSelecionaPasta
+            // 
+            this.btnSelecionaPasta.Location = new System.Drawing.Point(319, 43);
+            this.btnSelecionaPasta.Name = "btnSelecionaPasta";
+            this.btnSelecionaPasta.Size = new System.Drawing.Size(191, 23);
+            this.btnSelecionaPasta.TabIndex = 1;
+            this.btnSelecionaPasta.Text = "Selecionar Pasta Origem";
+            this.btnSelecionaPasta.UseVisualStyleBackColor = true;
+            this.btnSelecionaPasta.Click += new System.EventHandler(this.btnSelecionaPasta_Click);
+            // 
+            // txtNomePasta
+            // 
+            this.txtNomePasta.Location = new System.Drawing.Point(92, 43);
+            this.txtNomePasta.Name = "txtNomePasta";
+            this.txtNomePasta.Size = new System.Drawing.Size(221, 20);
+            this.txtNomePasta.TabIndex = 2;
+            // 
+            // btnLerEstrutura
+            // 
+            this.btnLerEstrutura.Location = new System.Drawing.Point(319, 106);
+            this.btnLerEstrutura.Name = "btnLerEstrutura";
+            this.btnLerEstrutura.Size = new System.Drawing.Size(191, 23);
+            this.btnLerEstrutura.TabIndex = 4;
+            this.btnLerEstrutura.Text = "Exibir Estrutura de Pastas";
+            this.btnLerEstrutura.UseVisualStyleBackColor = true;
+            this.btnLerEstrutura.Click += new System.EventHandler(this.btnLerEstrutura_Click);
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(12, 135);
+            this.textBox.Multiline = true;
+            this.textBox.Name = "textBox";
+            this.textBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox.Size = new System.Drawing.Size(647, 173);
+            this.textBox.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 311);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Arquivos Encontrados: ";
+            // 
+            // lblArquivosEncontrados
+            // 
+            this.lblArquivosEncontrados.AutoSize = true;
+            this.lblArquivosEncontrados.Location = new System.Drawing.Point(127, 310);
+            this.lblArquivosEncontrados.Name = "lblArquivosEncontrados";
+            this.lblArquivosEncontrados.Size = new System.Drawing.Size(0, 13);
+            this.lblArquivosEncontrados.TabIndex = 7;
+            // 
+            // btnIniciarReducao
+            // 
+            this.btnIniciarReducao.Location = new System.Drawing.Point(319, 345);
+            this.btnIniciarReducao.Name = "btnIniciarReducao";
+            this.btnIniciarReducao.Size = new System.Drawing.Size(191, 23);
+            this.btnIniciarReducao.TabIndex = 8;
+            this.btnIniciarReducao.Text = "Iniciar Redução Imagens";
+            this.btnIniciarReducao.UseVisualStyleBackColor = true;
+            this.btnIniciarReducao.Click += new System.EventHandler(this.btnIniciarReducao_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(319, 374);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(191, 23);
+            this.progressBar1.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 328);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Arquivos Reduzidos:";
+            // 
+            // lblQtdReduzidos
+            // 
+            this.lblQtdReduzidos.AutoSize = true;
+            this.lblQtdReduzidos.Location = new System.Drawing.Point(126, 328);
+            this.lblQtdReduzidos.Name = "lblQtdReduzidos";
+            this.lblQtdReduzidos.Size = new System.Drawing.Size(0, 13);
+            this.lblQtdReduzidos.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(671, 443);
+            this.Controls.Add(this.lblQtdReduzidos);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.btnIniciarReducao);
+            this.Controls.Add(this.lblArquivosEncontrados);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.btnLerEstrutura);
+            this.Controls.Add(this.txtNomePasta);
+            this.Controls.Add(this.btnSelecionaPasta);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSelecionaPasta;
+        private System.Windows.Forms.TextBox txtNomePasta;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button btnLerEstrutura;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblArquivosEncontrados;
+        private System.Windows.Forms.Button btnIniciarReducao;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblQtdReduzidos;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BatchImageResizer
+{
+    public partial class Form1 : Form
+    {
+
+        private List<String> arquivosEncontrados = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSelecionaPasta_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog1 = new FolderBrowserDialog();            
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK) {
+                txtNomePasta.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void btnLerEstrutura_Click(object sender, EventArgs e)
+        {
+            if (txtNomePasta.Text != null && txtNomePasta.Text.Length > 0)
+            {
+                lblArquivosEncontrados.Text = "";
+                arquivosEncontrados.Clear();
+                folderSearch(txtNomePasta.Text);
+                lblArquivosEncontrados.Text = arquivosEncontrados.Count.ToString();
+            }
+        }
+
+        private void folderSearch(String fileName)
+        {            
+            if (Directory.Exists(fileName))
+            {                
+                String[] arquivos = Directory.GetFiles(fileName, "*.jpg", SearchOption.AllDirectories);
+                arquivosEncontrados.AddRange(arquivos);
+                textBox.AppendText(arquivos.Count().ToString() + " arquivos localizados\n");                
+
+                //foreach (String arquivo in arquivos)
+                //{
+                //    textBox.AppendText("file: " + arquivo);
+                //    textBox.AppendText("\n");
+                //        arquivosEncontrados.Add(arquivo);
+            }
+            //    String[] subPastas = Directory.GetDirectories(fileName);
+            //    foreach (String subPasta in subPastas)
+            //    {
+            //        folderSearch(subPasta);
+            //    }
+            //}
+            //else
+            //{
+            //    textBox.AppendText("PASTA NÃO ENCONTRADA: " + fileName);
+            //    textBox.AppendText("\n");                    
+            //}
+        }
+
+        private void btnIniciarReducao_Click(object sender, EventArgs e)
+        {
+            progressBar1.Maximum = 100000;
+            progressBar1.Step = 1;
+
+            long qtdReduzidos = 0;
+            foreach (String imagem in arquivosEncontrados)
+            {
+
+                if (new System.IO.FileInfo(imagem).Length < 307200)
+                {
+                    continue;    
+                }
+                
+                String imagemSemExtensao = imagem.Replace(".jpg", "");
+                String nomeCopia = imagemSemExtensao + ".original.jpg";
+                File.Copy(imagem, nomeCopia);
+                File.Delete(imagem);
+
+                Image image = Image.FromFile(nomeCopia);
+                Image newImage = ScaleImage(image, 400, 500);
+                newImage.Save(imagem, ImageFormat.Jpeg);
+                image.Dispose();
+                newImage.Dispose();
+                File.Delete(nomeCopia);
+
+                progressBar1.PerformStep();
+                lblQtdReduzidos.Text = (++qtdReduzidos).ToString();
+                
+                // First load the image somehow
+                // Image myImage = Image.FromFile(nomeCopia, true);
+                // Save the image with a quality of 50% 
+                // SaveJpeg(imagem, myImage, 50); 
+            }
+        }
+
+        public static Image ScaleImage(Image image, int maxWidth, int maxHeight)
+        {
+            var ratioX = (double)maxWidth / image.Width;
+            var ratioY = (double)maxHeight / image.Height;
+            var ratio = Math.Min(ratioX, ratioY);
+
+            var newWidth = (int)(image.Width * ratio);
+            var newHeight = (int)(image.Height * ratio);
+
+            var newImage = new Bitmap(newWidth, newHeight);
+            Graphics.FromImage(newImage).DrawImage(image, 0, 0, newWidth, newHeight);
+            return newImage;
+        }
+
+        /// <summary> 
+        /// Saves an image as a jpeg image, with the given quality 
+        /// </summary> 
+        /// <param name="path"> Path to which the image would be saved. </param> 
+        /// <param name="quality"> An integer from 0 to 100, with 100 being the highest quality. </param> 
+        public static void SaveJpeg(string path, Image img, int quality)
+        {
+            if (quality < 0 || quality > 100)
+                throw new ArgumentOutOfRangeException("quality must be between 0 and 100.");
+
+            // Encoder parameter for image quality 
+            EncoderParameter qualityParam = new EncoderParameter(System.Drawing.Imaging.Encoder.Quality, quality);
+            // JPEG image codec 
+            ImageCodecInfo jpegCodec = GetEncoderInfo("image/jpeg");
+            EncoderParameters encoderParams = new EncoderParameters(1);
+            encoderParams.Param[0] = qualityParam;
+            img.Save(path, jpegCodec, encoderParams);
+        }
+
+        /// <summary> 
+        /// Returns the image codec with the given mime type 
+        /// </summary> 
+        private static ImageCodecInfo GetEncoderInfo(string mimeType)
+        {
+            // Get image codecs for all image formats 
+            ImageCodecInfo[] codecs = ImageCodecInfo.GetImageEncoders();
+
+            // Find the correct image codec 
+            for (int i = 0; i < codecs.Length; i++)
+                if (codecs[i].MimeType == mimeType)
+                    return codecs[i];
+
+            return null;
+        }
+
+        private static void ajustaDPI(String arquivo, String destino) {
+
+             using (Bitmap bitmap = (Bitmap)Image.FromFile(arquivo))
+                {
+                    using (Bitmap newBitmap = new Bitmap(bitmap))
+                    {
+                        newBitmap.SetResolution(300, 300);
+                        newBitmap.Save(destino, ImageFormat.Jpeg);
+                    }
+                }
+        }
+
+    }
+}
